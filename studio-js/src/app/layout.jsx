650ae7f6534478eb220fb6457aa8fcc135f6a8f0
Jeff Lortz <jefflortz@mac.mynetworksettings.com>
2026-06-24 22:45:04 -0400

    Add GTM script directly to metadata instead of using third-parties component

--- studio-js/src/app/layout.jsx
+++ studio-js/src/app/layout.jsx
@@ -1,18 +1,31 @@
 import '@/styles/tailwind.css'
-import { GoogleTagManager } from '@next/third-parties/google'
 
 export const metadata = {
   title: {
     template: '%s - Studio',
     default: 'Studio - Award winning developer studio based in Denmark',
   },
+  scripts: [
+    {
+      src: 'https://www.googletagmanager.com/gtag/js?id=GTM-NH3FZ5QQ',
+      async: true,
+    },
+  ],
 }
 
 export default function Layout({ children }) {
   return (
     <html lang="en" className="h-full bg-neutral-950 text-base antialiased">
       <head>
-        <GoogleTagManager gtmId="GTM-NH3FZ5QQ" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NH3FZ5QQ');`,
+          }}
+        />
       </head>
       <body className="flex min-h-full flex-col">{children}</body>
     </html>
