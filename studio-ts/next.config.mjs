7a50f255c9c1f6bf7a9d20756998985b500ef6ee
Jeff Lortz <jefflortz@mac.mynetworksettings.com>
2026-06-26 15:37:32 -0400

    Add redirect from www to non-www domain

--- studio-ts/next.config.mjs
+++ studio-ts/next.config.mjs
@@ -22,6 +22,21 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.coastalgrowthadvisor.com',
+          },
+        ],
+        destination: 'https://coastalgrowthadvisor.com/:path*',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 function remarkMDXLayout(source, metaName) {
